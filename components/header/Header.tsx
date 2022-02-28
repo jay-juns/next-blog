0d09ce0d0174a58775e70973f6d4c01fdeefdc75
jay-juns <necrozerg2@naver.com>
2022-03-01 06:49:03 +0900

    login

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -7,23 +7,37 @@ const Header = () => {
   return (
     <header>
       <nav>
-        <ul>
-          <li className={router.asPath === "/" ? "active" : ""}>
-            <Link href="/">
-              <a>Home</a>
+        <div className="left-nav">
+          <ul>
+            <li className={router.asPath === "/" ? "active" : ""}>
+              <Link href="/">
+                <a>Home</a>
+              </Link>
+            </li>
+            <li className={router.asPath === "/about" ? "active" : ""}>
+              <Link href="/about">
+                <a>About</a>
+              </Link>
+            </li>
+            <li className={router.asPath === "/contents" ? "active" : ""}>
+              <Link href="/contents">
+                <a>Contents</a>
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div className="right-nav">
+          <span className={router.asPath === "/login" ? "active" : ""}>
+            <Link href="/login">
+              Log In
             </Link>
-          </li>
-          <li className={router.asPath === "/about" ? "active" : ""}>
-            <Link href="/about">
-              <a>About</a>
+          </span>
+          <span className={router.asPath === "/signup" ? "active" : ""}>
+            <Link href="/signup">
+              Sign Up
             </Link>
-          </li>
-          <li className={router.asPath === "/contents" ? "active" : ""}>
-            <Link href="/contents">
-              <a>Contents</a>
-            </Link>
-          </li>
-        </ul>
+          </span>
+        </div>
       </nav>
     </header>
   )
