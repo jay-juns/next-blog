8f55b96f385839bf7a7e100ec7e847da19131157
jay-juns <necrozerg2@naver.com>
2022-03-12 18:08:30 +0900

    로고 변경

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -21,9 +21,9 @@ const Header = ({ children }: any) => {
           <Link href="/">
             <a className="home-logo-image">
               {theme === 'dark' ?
-               <img src="/images/logo/logo_ebsi_d.png" alt="logo" />
+               <img src="/images/logo/logo-d.png" alt="logo" />
                 :
-                <img src="/images/logo/logo_ebsi_w.png" alt="logo" /> 
+                <img src="/images/logo/logo-w.png" alt="logo" /> 
               }
             </a>
           </Link>
