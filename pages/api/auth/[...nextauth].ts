f5f61e89e0f385588e0a0d53f21f34667a8b7261
jay-juns <necrozerg2@naver.com>
2022-03-12 22:26:08 +0900

    login 이슈 해결중

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,17 +1,51 @@
 import NextAuth from 'next-auth'
 import GithubProvider from 'next-auth/providers/github'
-import  EmailProvider  from 'next-auth/providers/email'
+import CredentialsProvider from "next-auth/providers/credentials";
 import { PrismaAdapter } from '@next-auth/prisma-adapter'
 import { PrismaClient } from '@prisma/client'
+import { verifyPassword } from '../../../types/auth'
 
 const prisma = new PrismaClient()
 
 export default NextAuth({
   adapter: PrismaAdapter(prisma),
   providers: [
+    CredentialsProvider({
+      name: "유저 이메일, 패스워드 방식",
+      credentials: {
+        email: { label: "유저 이메일", type: "email", placeholder: "user@email.com"},
+        password: { label: "패스워드", type: "password"}
+      },
+        async authorize(credentials) {
+          const user = await prisma.user.findUnique({
+              where: {
+                  email: String(credentials.email),
+              },
+              select: {
+                  name: true, email: true, password: true
+              },
+          });
+
+          if (!user) {
+              throw new Error('No user found!');
+          }
+
+          const isValid = await verifyPassword(
+              credentials.password,
+              user.password
+          );
+
+          if (!isValid) {
+              throw new Error('Could not log you in!');
+          }
+          return { name: user.name, email: user.email };
+      }
+      
+    }),
     GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
     })
-  ]
+  ],
+  secret: process.env.SECRET,
 })
--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,15 +1,57 @@
-import React, { FC } from 'react'
+import React, { FC, useState, useRef } from 'react'
 import Head from 'next/head'
-import { signIn } from 'next-auth/react'
 import PageWithLayoutType from '../../types/pageWithLayoutType';
 import DefaultLayout from '../../components/layouts/Default';
+import { useSession, signIn } from "next-auth/react";
+import { useRouter } from "next/router"
 
 const Login: FC = () => {
+  const [formStatus, setFormStatus] = useState<string>();
+
+  const emailInputRef = useRef<HTMLInputElement>(null);
+  const passwordInputRef = useRef<HTMLInputElement>(null);
+
+  async function submitHandler(event: React.SyntheticEvent) {
+    event.preventDefault();
+
+    const enteredEmail = emailInputRef.current?.value;
+    const enteredPassword = passwordInputRef.current?.value;
+    
+    try {
+      const result = await signIn("credentials", {
+        redirect: false,
+        email: enteredEmail,
+        password: enteredPassword,
+      });
+      console.log(result);
+      setFormStatus(`log in: ${result.message}`);
+      router.replace("/dashboard")
+    } catch(e: any) {
+      console.log(e);
+      setFormStatus(`Error Occured: ${e.message}`);
+    }
+  }
+
+  const { data: session, status } = useSession();
+  const router = useRouter();
+
   const handleSignIn = async () => {
     await signIn('github', {
       callbackUrl: 'http://localhost:3000/dashboard'
     })
   }
+  
+  if (status === "authenticated") {
+    router.replace("/");
+    return (
+      <div>
+        <h1>Log in</h1>
+        <div>You are already logged in.</div>
+        <div>Now redirect to main page.</div>
+      </div>
+    );
+  }
+
 
   return (
     <div className="main-container">
@@ -19,6 +61,38 @@ const Login: FC = () => {
       </Head>
       <h1>Login</h1>
       <div>
+        <div>
+          <form onSubmit={submitHandler}>
+            <div>
+              <label htmlFor="email">
+                Email
+              </label>
+              <input 
+                id="email"
+                type="text" 
+                placeholder="Email"
+                required
+                ref={emailInputRef}  
+              />
+            </div>
+            <div>
+              <label htmlFor="password">
+                 Password
+              </label>
+              <input 
+                id="password"
+                type="password" 
+                placeholder="Password"
+                required
+                ref={passwordInputRef}  
+              />
+            </div>
+            <p>{formStatus}</p>
+            <div>
+              <button className='btn' type="submit">Log in</button>
+            </div>
+          </form>
+        </div>
         <button onClick={handleSignIn}>github login</button>
       </div>
     </div>
